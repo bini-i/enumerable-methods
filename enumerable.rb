202c32173482969927a742035d0c68ccb791d003
dibdas <dibyandu08das@gmail.com>
2020-10-19 17:27:02 +0530

    add my_each

--- enumerable.rb
+++ enumerable.rb
@@ -2,14 +2,14 @@
 
 module Enumerable
   def my_each()
-    self.length.times do |k|
-      result = yield self[k] if block?
+    length.times do |k|
+      result = yield self[k]
       puts result
     end
     self
   end
 end
 
-my_each([8, 3, 6, 8, 4].my_each) do |i|
+[8, 3, 6, 8, 4,78,56].my_each do |i|
   i
 end
