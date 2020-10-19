df9b8296adb8105086f61e49ad5699d98906dcf0
dibdas <dibyandu08das@gmail.com>
2020-10-19 20:07:33 +0530

    add my_index

--- enumerable.rb
+++ enumerable.rb
@@ -1,15 +1,40 @@
 
 
 module Enumerable
-  def my_each()
-    length.times do |k|
-      result = yield self[k]
-      puts result
+    def my_each()
+        self.length.times do |k|
+        result = yield self[k]
+        puts result
+        end
+        self
     end
-    self
-  end
-end
+ 
+    def my_index()
+        puts "index"
+        self.length.times do |j|
+            yield j
+        end
+        self
+    end
 
-[8, 3, 6, 8, 4,78,56].my_each do |i|
-  i
+    def my_select()
+        puts "odd"
+        selected_elements=Array.new
+        self.my_each() do |k|
+            selected_elements.push(k) if (yield k)
+        end
+        puts selected_elements
+    end
+end
+    
+
+
+
+ 
+ 
+
+   # puts selected_elements
+ 
+[8, 3, 6, 81, 4,78,24,45,91].my_select do |i| 
+     i.odd?
 end
