73ba16277e311b06cde1657a6ba1efa6a24501de
bini-i <binyam.sh1@gmail.com>
2020-10-20 17:29:52 +0300

    Add git workflows

--- _enumerable.rb
+++ _enumerable.rb
@@ -1,77 +1,75 @@
-
-
 module Enumerable
-    def my_each
-        for ele in self
-            yield ele
-        end
-        self
+  def my_each
+    each do |ele|
+      yield ele
     end
- 
-    def my_each_with_index
-        self.length.times do |j|
-            yield j
-        end
-        self
+    self
+  end
+
+  def my_each_with_index
+    length.times do |j|
+      yield j
+    end
+    self
+  end
+
+  def my_select
+    selected_elements = []
+    my_each do |k|
+      selected_elements.push(k) if yield k
+    end
+    selected_elements
+  end
+
+  def my_all?
+    my_each do |ele|
+      # returns false if an element that doesn't satsify the condition is found
+      return false unless yield ele
+    end
+    true
+  end
+
+  def my_any?
+    my_each do |ele|
+      return true if yield ele
+    end
+    false
+  end
+
+  def my_none?
+    my_each do |ele|
+      return false if yield ele
+    end
+    true
+  end
+
+  def my_count
+    count = 0
+    my_each do |ele|
+      count += 1 if yield ele
+    end
+    count
+  end
+
+  def my_map(*param, &block)
+    new_array = []
+    if param.length.positive?
+      proc = param[0] # proc object
+    elsif param.length.zero? && block
+      proc = block # proc object
     end
 
-    def my_select
-        selected_elements = []
-        self.my_each do |k|
-            selected_elements.push(k) if (yield k)
-        end
-        selected_elements
+    my_each do |ele|
+      new_array.push(proc.call(ele))
     end
-    
-    def my_all?
-        self.my_each do |ele|              
-            #returns false if an element that doesn't satsify the condition is found     
-            return false unless yield ele
-        end
-        true         
-    end
+    new_array
+  end
 
-    def my_any?
-        self.my_each do |ele|
-            return true if yield ele
-        end
-        false
+  def my_inject
+    accumulator = first
+    (1..(length - 1)).my_each do |indx|
+      accumulator = yield accumulator, self[indx]
     end
-
-    def my_none?
-        self.my_each do |ele|
-            return false if yield ele
-        end
-        true
-    end
-
-    def my_count
-        count=0
-        self.my_each do |ele|
-            count+=1 if yield ele
-        end
-        count
-    end
-
-    def my_map(*param, &block)
-        new_array=[]
-        if param.length>0 
-            proc = param[0]     #proc object
-        elsif param.length == 0 && block
-            proc = block        #proc object
-        end
-
-        self.my_each do |ele|
-            new_array.push(proc.call(ele)) 
-        end
-        new_array
-    end
-
-    def my_inject
-        accumulator=self.first
-        (1..(self.length-1)).my_each do |indx|
-            accumulator = yield accumulator,self[indx]
-        end
-        accumulator
-    end
+    accumulator
+  end
 end
