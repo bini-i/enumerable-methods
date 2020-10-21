05f800caa00812326b348a7f6c652b5d97c6c3e3
bini-i <binyam.sh1@gmail.com>
2020-10-21 15:38:27 +0300

    Update my_all_method
    to check when argument of type Regexp and class is passed
    to return true when no block is passed and all elements are truthy
    to return false when no block is passed and an element of falsy exists

--- _enumerable.rb
+++ _enumerable.rb
@@ -2,9 +2,9 @@ module Enumerable
   def my_each()
     return to_enum(:my_each) unless block_given?
 
-    arr = *self             #if a range is given, it splats it into an array
+    arr = *self # if a range is given, it splats it into an array
     size.times do |indx|
-      yield arr[indx]     
+      yield arr[indx]
     end
     self
   end
@@ -12,7 +12,7 @@ module Enumerable
   def my_each_with_index
     return to_enum(:my_each) unless block_given?
 
-    arr = *self             #if a range is given, it splats it into an array
+    arr = *self # if a range is given, it splats it into an array
     size.times do |indx|
       yield arr[indx], indx
     end
@@ -20,6 +20,8 @@ module Enumerable
   end
 
   def my_select
+    return to_enum(:my_each) unless block_given?
+
     selected_elements = []
     my_each do |k|
       selected_elements.push(k) if yield k
@@ -27,7 +29,25 @@ module Enumerable
     selected_elements
   end
 
-  def my_all?
+  def my_all?(*param)
+    if param.length.positive?
+      if param[0].class == Regexp
+        my_each do |ele|
+          return false unless param[0].match(ele)
+        end
+        return true
+      elsif param[0].class == Class
+        my_each do |ele|
+          return false unless ele.is_a?(param[0])
+        end
+        return true
+      end
+    end
+
+    # returns false if a block is not given and falsy element is found
+    my_each { |ele| return false unless ele || block_given? }
+    return true unless block_given?
+
     my_each do |ele|
       # returns false if an element that doesn't satsify the condition is found
       return false unless yield ele
