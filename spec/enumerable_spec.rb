d96d7faa08481484dda531e2ac880bc6d4fa940e
bini-i <binyam.sh1@gmail.com>
2020-11-03 16:13:55 +0300

    Add test cases for my_each_with_index method
    - tests return result and iteration

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -21,6 +21,24 @@ describe Enumerable do
         end
 
         it "returns the original array" do
+        expect(array.my_each{}).to eq(array)
+        end
+    end
+
+    describe "#my_each_with_index" do
+        it "iterates through each elements of a range with index" do
+            expect(range.my_each_with_index{|x, i| my_result << [x, i]}).to eq(range.each_with_index{|x, i| result << [x, i]})
+        end
+        
+        it "iterates through each elements of an array with index" do
+            expect(array.my_each_with_index{|x, i| my_result << [x, i]}).to eq(array.each_with_index{|x, i| result << [x, i]})
+        end
+
+        it "returns an enumerator when no block is passed" do
+            expect(array.my_each.is_a?(Enumerator)).to eq(true)
+        end
+
+        it "returns the original array" do
         expect(array.my_each{}).to eq(array.each{})
         end
     end
